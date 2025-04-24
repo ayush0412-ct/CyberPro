3fb1ca9ad2f7348d70a1fc20b9af7e5de6b09c85
ayush0412-ct <ayushshaeed@gmail.com>
2025-04-24 20:05:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+  });
 // MongoDB Setup
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
